11645789f1f84f5d419aaaeb1d40acc5503bc3ca
Al Fajr <62805347+aN4ksaL4y@users.noreply.github.com>
2024-03-18 01:13:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 def random_id():
   return os.system("""python -c "import secrets; print(secrets.token_hex()[:7])"""")
-                   
+
 def random_uid():
   return os.system("""python -c "import uuid; print(uuid.uuid4())"""")
 
