5971c6a119bd3630a5a02ed5fba30a414ef73338
Your Name <you@example.com>
2024-03-18 01:09:40 +0800

    kasi komen

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ def random_id():
 def random_uid():
   return os.system("""python -c "import uuid; print(uuid.uuid4())"""")
 
+# kadang pas mau rename file ingin rapi, jadi perlu ganti spasi jadi '-'
 def remove_special_char():
   return os.system("""python -c "import re;print(re.sub(r'[^a-zA-Z0-9]+', '-', '').lower())"""")
